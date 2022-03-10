c2c49fce64bbfd31f29851c7a415b7b86b410353
NimiKnight <NimiKnight@gmail.com>
2022-03-10 04:27:57 +0330

    Create Structure

---
+++ src/data/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+PATH_DIR = Path(__file__).resolve().parent
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import shutil
+from pathlib import Path
+import sys
+
+from loguru import logger
+
+from src.data import PATH_DIR
+
+class OrginizeDirectory:
+    pass
+
+if __name__ == '__main__':
+    pass
